d85ac45548335abefd0439b97c3e8c4e3b657b5c
xiaxuan <xiaxuan@haizhi.com>
2016-04-12 14:22:35 +0800

    添加mybatis配置

---
+++ src/main/java/cn/com/config/DataSourceFactory.java
@@ -0,0 +1,45 @@
+package cn.com.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by xiaxuan on 16/4/12.
+ */
+@MapperScan("cn.com.mapper")
+@ComponentScan
+public class DataSourceFactory {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return new org.apache.tomcat.jdbc.pool.DataSource();
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactoryBean() throws Exception {
+
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource());
+
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+
+        sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath:/mybatis/*.xml"));
+
+        return sqlSessionFactoryBean.getObject();
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(dataSource());
+    }
+}
---
+++ src/main/java/cn/com/mapper/ProductMapper.java
@@ -0,0 +1,13 @@
+package cn.com.mapper;
+
+import cn.com.domain.Product;
+
+import java.util.List;
+
+/**
+ * Created by xiaxuan on 16/4/12.
+ */
+public interface ProductMapper {
+
+    List<Product> listAll();
+}
