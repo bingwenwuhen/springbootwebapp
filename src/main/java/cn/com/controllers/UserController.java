ef5974ff6f0a4f0e5c7992912095c2bf7b7853ba
xiaxuan <xiaxuan@haizhi.com>
2016-04-29 11:43:11 +0800

    写一个demo

--- src/main/java/cn/com/controllers/UserController.java
+++ src/main/java/cn/com/controllers/UserController.java
@@ -5,9 +5,12 @@ import cn.com.mapper.UserMapper;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.websocket.server.PathParam;
 
 /**
  * Created by Administrator on 2016/4/12.
@@ -38,5 +41,12 @@ public class UserController {
     @ApiOperation("测试mongo是否整合成功")
     public void testMongo() {
         System.out.println(mongoTemplate);
+    }
+
+    @RequestMapping(value = "/hello/{hello}", method = RequestMethod.GET,
+    produces = "application/json;charset=utf-8")
+    @ApiOperation("测试hello")
+    public String sayHello(@PathVariable @PathParam("hello") String hello) {
+        return "hello " + hello;
     }
 }
