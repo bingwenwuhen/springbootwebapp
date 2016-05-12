4fd353013960f43f6d48a264cf10a0decd609363
bingwenwuhen <bingwenwuhen@163.com>
2016-05-12 15:11:46 +0800

    整合morphia成功

---
+++ src/main/java/cn/com/config/MorphiaFactory.java
@@ -0,0 +1,32 @@
+package cn.com.config;
+
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.Morphia;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.mongo.MongoProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.inject.Inject;
+
+/**
+ * Created by Administrator on 2016/5/12.
+ */
+@Configuration
+@ConditionalOnClass(Mongo.class)
+public class MorphiaFactory {
+
+    @Inject
+    private Mongo mongo;
+
+    @Inject
+    MongoProperties mongoProperties;
+
+    @Bean
+    public Datastore get() {
+        Morphia morphia = new Morphia();
+        return morphia.createDatastore((MongoClient) mongo,mongoProperties.getDatabase());
+    }
+}
--- src/main/java/cn/com/controllers/IndexController.java
+++ src/main/java/cn/com/controllers/IndexController.java
@@ -1,8 +1,11 @@
 package cn.com.controllers;
 
+import org.mongodb.morphia.Datastore;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.inject.Inject;
 
 /**
  * Created by Administrator on 2016/4/11.
@@ -11,8 +14,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @EnableAutoConfiguration
 public class IndexController {
 
+    @Inject
+    private Datastore datastore;
+
     @RequestMapping("/")
     String index() {
+        System.out.println(datastore);
         return "index";
     }
 }
